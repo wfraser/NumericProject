8d6a38699e582640d14f1005407f979823498c5d
William R. Fraser <wfraser@codewise.org>
2015-04-25 23:55:01 -0700

    reorganize the test code

--- NumericProject/NumericProject.cpp
+++ NumericProject/NumericProject.cpp
@@ -7,34 +7,66 @@ using namespace std;
 
 int wmain(int argc, wchar_t* argv[])
 {
-    stringstream out;
+    if (false)
+    {
+        // This would fail because it will overflow the overflow segment.
+        // It involves doing 100 * 5 as the last step, which is too big for a uint8_t.
+        // I think multiplication on overflowed numbers should be forbidden.
+        BCD<uint8_t> aa(0xFF);
+        BCD<uint8_t> bb(0xFF);
+        BCD<uint8_t> cc = aa * bb;
+    }
 
-    BCD<uint8_t> a(0xFF);
-    BCD<uint8_t> b(0xFF);
-    BCD<uint8_t> c1 = a + b;
-    BCD<uint8_t> c2 = c1.GetAndClearOverflow();
+    // Test on BCD<uint8_t>
+    {
+        stringstream out;
 
-    c2.Print(out);
-    c1.Print(out, true);
-    assert(out.str() == "510");
-    out.str("");
+        BCD<uint8_t> a(0xFF);
+        BCD<uint8_t> b(0xFF);
+        BCD<uint8_t> c1 = a + b;
+        BCD<uint8_t> c2 = c1.GetAndClearOverflow();
 
-    BCD<uint16_t> x(0xFF7F);
-    BCD<uint16_t> y(0xFF7F);
-    BCD<uint16_t> z1 = x + y;
-    BCD<uint16_t> z2 = z1.GetAndClearOverflow();
+        c2.Print(out);
+        c1.Print(out, true);
+        assert(out.str() == "510");
+    }
 
-    z2.Print(out);
-    z1.Print(out, true);
-    assert(out.str() == "130814");
-    out.str("");
+    // Test on BCD<uint16_t>
+    {
+        stringstream out;
 
-    BigInt<BCD<uint16_t>> xyz(0xFF7F);
-    xyz += BCD<uint16_t>(0xFF7F);
+        BCD<uint16_t> a(0xFF7F);
+        BCD<uint16_t> b(0xFF7F);
+        BCD<uint16_t> c1 = a + b;
+        BCD<uint16_t> c2 = c1.GetAndClearOverflow();
 
-    xyz.Print<10>(out);
-    assert(out.str() == "130814");
-    out.str("");
+        c2.Print(out);
+        c1.Print(out, true);
+        assert(out.str() == "130814");
+    }
+
+    // Test printing a BigInt<BCD>
+    {
+        stringstream out;
+
+        BigInt<BCD<uint16_t>> xyz(0xFF7F);
+        xyz += BCD<uint16_t>(0xFF7F);
+
+        xyz.Print<10>(out);
+        assert(out.str() == "130814");
+    }
+
+    // Test printing a BigInt<uint16_t>
+    // Internally it converts to a BigInt<BCD<uint16_t, 10>> for printing.
+    {
+        stringstream out;
+
+        BigInt<uint16_t> printTest(0xFF7F);
+        printTest += 0xFF7F;
+
+        printTest.Print<10>(out);
+        assert(out.str() == "130814");
+    }
 
     return 0;
 }
